42ce03c0af8bd7f996b96b9891fbec2a26da8c09
inorimylove <1506732992@qq.com>
2020-04-14 21:28:16 +0800

    init

--- src/main/java/me/xiaohuang/companyframe/controller/IndexController.java
+++ src/main/java/me/xiaohuang/companyframe/controller/IndexController.java
@@ -1,5 +1,11 @@
 package me.xiaohuang.companyframe.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * @ClassName IndexController
  * @Description IndexController
@@ -7,5 +13,14 @@ package me.xiaohuang.companyframe.controller;
  * @Date 2020/4/10 12:09
  * @Version 1.0
  */
+@Controller
+@RequestMapping("/index")
+@Api(tags = "视图", description = "跳转视图的控制器")
 public class IndexController {
+
+    @GetMapping("/404")
+    @ApiOperation(value = "跳转404错误页面")
+    public String error404() {
+        return "error/404";
+    }
 }
--- src/main/java/me/xiaohuang/companyframe/mapper/SysUserMapper.java
+++ src/main/java/me/xiaohuang/companyframe/mapper/SysUserMapper.java
@@ -1,12 +1,7 @@
 package me.xiaohuang.companyframe.mapper;
 
 import me.xiaohuang.companyframe.entity.SysUser;
-import me.xiaohuang.companyframe.vo.req.UserPageReqVO;
-import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
-@Repository
 public interface SysUserMapper {
     int deleteByPrimaryKey(String id);
 
@@ -19,8 +14,4 @@ public interface SysUserMapper {
     int updateByPrimaryKeySelective(SysUser record);
 
     int updateByPrimaryKey(SysUser record);
-
-    SysUser getUserInfoByName(String userName);
-
-    List<SysUser> selectAll(UserPageReqVO vo);
 }
